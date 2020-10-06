2de63a079f8ca9406613f71836b658bfced5d8ca
Kenneth Carroll <kenneth.james.carroll@gmail.com>
2020-10-06 18:22:05 -0400

    Updates

---
+++ precache-manifest.c190e1046fed69f3e7d1dda1f22f7cbc.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb63d393abfa5073d161625fa1ff0159",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2a4ef7477b6b530e0b1",
+    "url": "/static/css/main.df5a74af.chunk.css"
+  },
+  {
+    "revision": "d45a55b7c29d5e47eca7",
+    "url": "/static/js/2.e56bfaa7.chunk.js"
+  },
+  {
+    "revision": "0d6c4affe2a659999d44765f9a17cb4f",
+    "url": "/static/js/2.e56bfaa7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2a4ef7477b6b530e0b1",
+    "url": "/static/js/main.f9972706.chunk.js"
+  },
+  {
+    "revision": "b21ca5060ebf90f9cfa7",
+    "url": "/static/js/runtime-main.4a58ef6b.js"
+  },
+  {
+    "revision": "ffd1d7056c54a815db0f106ec351ffaf",
+    "url": "/static/media/about.ffd1d705.jpg"
+  },
+  {
+    "revision": "64a3fd7590b5db223724cf2a893ed381",
+    "url": "/static/media/angularjs.64a3fd75.svg"
+  },
+  {
+    "revision": "fa6891f4ccb699b50efa11fb7f5e0b76",
+    "url": "/static/media/c++.fa6891f4.svg"
+  },
+  {
+    "revision": "580ee73ec52221383f3da4f5dad0af3d",
+    "url": "/static/media/c.580ee73e.svg"
+  },
+  {
+    "revision": "bb5f7ebde62d990d38f015ba76be0e59",
+    "url": "/static/media/javascript.bb5f7ebd.svg"
+  },
+  {
+    "revision": "7456989b12166f15bb3d7d245e4d843b",
+    "url": "/static/media/mac-os.7456989b.svg"
+  },
+  {
+    "revision": "e4ddbe1a57d161bfb593c7e2ba40d345",
+    "url": "/static/media/nodejs.e4ddbe1a.svg"
+  },
+  {
+    "revision": "d774197a2e1f31b3d3986464547c8917",
+    "url": "/static/media/profile.d774197a.jpg"
+  },
+  {
+    "revision": "4948e6764d2214fe0c09621c6defe06b",
+    "url": "/static/media/python.4948e676.svg"
+  },
+  {
+    "revision": "a899d525422ddb1c9b5bc595ec6da942",
+    "url": "/static/media/react-native.a899d525.svg"
+  },
+  {
+    "revision": "62724d7d6bbdc29d3f251e4182a5084f",
+    "url": "/static/media/windows-10.62724d7d.svg"
+  }
+]);
