847de70c5ce97edba734ab118d723cb95668d61a
Kenneth Carroll <kenneth.james.carroll@gmail.com>
2022-03-29 14:48:00 -0400

    Updated menu and created page templates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,13 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './shared/menu/menu.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { ArticlesComponent } from './pages/articles/articles.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
-  declarations: [AppComponent, MenuComponent],
+  declarations: [AppComponent, MenuComponent, AboutComponent, ProjectsComponent, ArticlesComponent, ContactComponent],
   imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
